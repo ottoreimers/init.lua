9a06bc252281de655df11df70ad399bf067faac5
Otto Reimers <otto.reimers@gmail.com>
2026-04-07 14:28:18 +0200

    Fix some config stuff

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
@@ -10,7 +10,6 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 vim.opt.rtp:prepend(lazypath)
-vim.opt.termguicolors = true
 vim.wo.number = true
 vim.wo.relativenumber = true
 require("vim-cmd-options")
